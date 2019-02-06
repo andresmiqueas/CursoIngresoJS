8ec7df611b7174ae20b66ba270227d8bb36c4c16
alumno <alumno>
2019-02-06 11:00:25 -0300

    primer comando de consola

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,18 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var numerouno;
+var numerodos;
+var suma;
+
+numerouno=numeroUno.value;
+
+numerouno=parseInt(numerouno);
+numerodos=parseInt(numerodos);
+
+
+suma= numerouno + numerodos;
+ALERT ("El resultado es ") + suma;
 
 }
 
